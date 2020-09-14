351585ae3f696f1ab377e04e40a6e22f909a6da0
handarel <sebastian.berg@handarel.io>
2020-09-14 18:39:23 +0200

    feat: add more file extensions

--- packages/eslint-config/src/lib/node.ts
+++ packages/eslint-config/src/lib/node.ts
@@ -1,6 +1,11 @@
 import type { Linter } from 'eslint';
 
 const config: Linter.Config = {
+  settings: {
+    node: {
+      tryExtensions: ['.js', '.ts', '.jsx', '.tsx', '.mjs', '.json', '.node'],
+    },
+  },
   extends: ['plugin:node/recommended'],
 };
 

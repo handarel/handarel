1d87f8f9f1a60ba3e36097ab61f5f94d0436f238
handarel <sebastian.berg@handarel.io>
2020-09-14 12:41:58 +0200

    feat: create package eslint-config-typescript

---
+++ packages/eslint-config-typescript/README.md
@@ -0,0 +1,7 @@
+# eslint-config-typescript
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test eslint-config-typescript` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/eslint-config-typescript/src/index.ts
@@ -0,0 +1,16 @@
+import { Linter } from 'eslint';
+
+const config: Linter.Config = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [].map((path) => require.resolve(path)),
+};
+
+export = config;

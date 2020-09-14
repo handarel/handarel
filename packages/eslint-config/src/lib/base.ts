cd0755d0220492efb4698693a70d0ec76751d569
handarel <sebastian.berg@handarel.io>
2020-09-14 15:18:33 +0200

    feat: add rules for default-param and max-len

--- packages/eslint-config/src/lib/base.ts
+++ packages/eslint-config/src/lib/base.ts
@@ -3,9 +3,27 @@ import { Linter } from 'eslint';
 const config: Linter.Config = {
   extends: ['airbnb-base'],
   rules: {
+    // This rule enforces default parameters to be the last of parameters
+    // https://eslint.org/docs/rules/default-param-last
+    'default-param-last': 'error',
+
     // Require or disallow an empty line between class members
     // https://eslint.org/docs/rules/lines-between-class-members
     'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+
+    // This rule enforces a maximum line length to increase code readability and maintainability
+    // https://eslint.org/docs/rules/max-len
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignoreUrls: true,
+        ignoreComments: false,
+        ignoreRegExpLiterals: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
   },
 };
 

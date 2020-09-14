4e916dc3ef5ecf3fd8383a281ad729f2374d453d
handarel <sebastian.berg@handarel.io>
2020-09-14 12:30:52 +0200

    feat: add imports ruleset

---
+++ packages/eslint-config/src/lib/imports.ts
@@ -0,0 +1,45 @@
+import { Linter } from 'eslint';
+
+const config: Linter.Config = {
+  plugins: ['simple-import-sort'],
+  extends: ['plugin:import/errors', 'plugin:import/warnings'],
+  rules: {
+    // Disable for 'simple-import-sort'
+    'sort-imports': 'off',
+
+    // Disable for 'simple-import-sort'
+    'import/order': 'off',
+
+    // Require modules with a single export to use a default export
+    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
+    'import/prefer-default-export': 'off',
+
+    // Specifies the order of imports for custom grouping
+    // https://github.com/lydell/eslint-plugin-simple-import-sort#custom-grouping
+    'simple-import-sort/sort': [
+      'error',
+      {
+        groups: [
+          // Node builtins first.
+          [`^(${require('module').builtinModules.join('|')})(/|$)`],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything that does not start with a dot.
+          ['^[^.]'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
+  },
+};
+
+export = config;

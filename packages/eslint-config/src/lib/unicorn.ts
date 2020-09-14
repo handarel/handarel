267cbf1ef6436ddf1b7b4c0ae7527607c2e612dd
handarel <sebastian.berg@handarel.io>
2020-09-14 17:18:54 +0200

    feat: add rule description and url

--- packages/eslint-config/src/lib/unicorn.ts
+++ packages/eslint-config/src/lib/unicorn.ts
@@ -3,6 +3,8 @@ import type { Linter } from 'eslint';
 const config: Linter.Config = {
   extends: ['plugin:unicorn/recommended'],
   rules: {
+    // This rule prevents the use of abbreviations for more readable code
+    // https://github.com/sindresorhus/eslint-plugin-unicorn/blob/master/docs/rules/prevent-abbreviations.md
     'unicorn/prevent-abbreviations': [
       'error',
       {
